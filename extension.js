d192930fa35f8d1fc621d8a0f9d19e249d820603
Zachary Lineman <zachary.lineman@gmail.com>
2021-02-13 01:00:52 -0500

    Added the Jelly language and made it more user freindly.

--- extension.js
+++ extension.js
@@ -1,45 +1,66 @@
 const vscode = require("vscode");
 const WebSocket = require("ws");
 const path = require("path");
-const { OutputFileType, createPrinter } = require("typescript");
 const port = 8080;
 const internalIp = require('internal-ip');
 
 const output = vscode.window.createOutputChannel("Jellycuts");
 var cacheWS = null
+var cacheWSS = null
 output.show(true);
 
+//onCommand:
 function activate(context) {
-  const wss = new WebSocket.Server({ port: port });
-  wss.setMaxListeners(1);
-  wss.setMaxListeners(1)
+  const runServerCommand = 'jellycuts-support.runserver';
 
-  vscode.window.showInformationMessage(
-    `Jellycuts bridge opened on port ${internalIp.v4.sync()}:${port}`
-  );
-
-  wss.on("connection", function connection(ws, req) {
-    cacheWS = ws
-    vscode.window.showInformationMessage(`Client Connected -> ${req.connection.remoteAddress}`);
-
-    ws.on("message", function incoming(message) {
-      if(message == "run") {
-        updateApp(ws, "run")
-      } else if (message == "export") {
-        updateApp(ws, "export")
-      } else if (message == "download") {
-        updateApp(ws, "download")
-      } else if (message == "closing") {
-        vscode.window.showInformationMessage(`Client Disconnected -> ${req.connection.remoteAddress}`);
-        cacheWS = null
-      } else if (message != "ping") {
-        output.append(`${message}\n`);
-      }
+  const runServerHandler = () => {
+    const wss = new WebSocket.Server({ port: port });
+    cacheWSS = wss
+    wss.setMaxListeners(1);
+    
+    vscode.window.showInformationMessage(
+      `Jellycuts bridge opened on port ${internalIp.v4.sync()}:${port}`
+    );
+  
+    wss.on("connection", function connection(ws, req) {
+      vscode.window.showInformationMessage(`Client Connected -> ${req.connection.remoteAddress}`);
+  
+      ws.on("message", function incoming(message) {
+        if(message == "run") {
+          updateApp(ws, "run")
+        } else if (message == "export") {
+          updateApp(ws, "export")
+        } else if (message == "download") {
+          updateApp(ws, "download")
+        } else if (message == "closing") {
+          vscode.window.showInformationMessage(`Client Disconnected -> ${req.connection.remoteAddress}`);
+          cacheWS = null
+        } else if (message != "ping") {
+          output.append(`${message}\n`);
+        }
+      });
     });
-  });
+  };
+  context.subscriptions.push(vscode.commands.registerCommand(runServerCommand, runServerHandler));
+
+  const closeServerCommand = 'jellycuts-support.closeserver';
+
+  const closeServerHandler = () => {
+    if (cacheWS != null) {
+      for(const client of cacheWSS.clients)
+      {
+        client.close();
+      }
+      vscode.window.showInformationMessage(`Acitve Jellycuts Bridges have been closed`);  
+    } else {
+      vscode.window.showInformationMessage(`There are no active connections to the Jellycuts Bridge`);  
+    }
+  };
+  context.subscriptions.push(vscode.commands.registerCommand(closeServerCommand, closeServerHandler));
 
   let runDisposable = vscode.commands.registerCommand('jellycuts-support.runOnDevice', () => {
     if (cacheWS != null) {
+      vscode.window.showInformationMessage(`Running Jellycut on connected device`);
       updateApp(cacheWS, "run")
     }
   });
@@ -48,6 +69,7 @@ function activate(context) {
   
   let exportDisposable = vscode.commands.registerCommand('jellycuts-support.exportOnDevice', () => {
     if (cacheWS != null) {
+      vscode.window.showInformationMessage(`Exporting Jellycut to Shortcuts`);
       updateApp(cacheWS, "export")
     }
   });
